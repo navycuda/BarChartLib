626520ee310bac2059ea70af258323c852a26662
Shane <navysupra@gmail.com>
2022-07-15 01:32:25 +0000

    learning how to create divs in a loop with a function

--- www/js/site.js
+++ www/js/site.js
@@ -1,5 +1,33 @@
 /* eslint-env es6 */
+// Do stuff once the document is ready.
 $(document).ready(function(){
+  const createDiv = function(text) {
+    const result = document.createElement("div");
+    result.innerText = text;
+    return result;
+  };
+
+
+  // main body
   const body = document.body;
-  body.append("Hello Deroche!");
+
+  const header = document.createElement("header");
+  const headerTitle = document.createElement("h1");
+
+  body.appendChild(header);
+  header.appendChild(headerTitle);
+
+  headerTitle.append("Hello Deroche!");
+
+  const divs = [
+    createDiv("div 1"),
+    createDiv("div 2"),
+    createDiv("div 3")
+  ];
+
+  for (let d = 0; d < divs.length; d++){
+    body.appendChild(divs[d]);
+  }
+
+
 });

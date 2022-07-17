de9d007c841293fac498fc421a863be72c852bd8
Shane <navysupra@gmail.com>
2022-07-17 03:09:30 +0000

    Working!  The table is animated via javascript but I might have to look at css animation as the javascript seems a bit slow.

--- www/js/site.js
+++ www/js/site.js
@@ -12,14 +12,49 @@ $(document).ready(function(){
 ///
 /// }
 /// element = document.element
+  const generateRow = function (value){
+    const row = document.createElement("div");
+    row.setAttribute("class", "_row");
+    row.setAttribute("data-value", value);
+    row.innerText = value;
+    return row;
+  };
+  const getPercent = function (value, min, max){
+    if (value >= max){
+      return 100;
+    } else if (value <= min){
+      return 0;
+    }
+    return (value / max) * 100;
+  };
   const drawBarChart = function (data, options, element) {
     // For starters, assume the types passed in are correct.
     // create the chart element and populate the body
     const chart = document.createElement("div");
     chart.setAttribute("class", "_chart");
     element.appendChild(chart);
-
-
+    // create the rows and populate with the data.
+    for (let d = 0; d < data.length; d++){
+      chart.appendChild(generateRow(data[d]));
+    }
+    // collect the rows in an array
+    const rows = document.getElementsByClassName('_row');
+    console.log(rows);
+    // iterate through to change percent
+    let percent = 0;
+    const animateChart = setInterval(() => {
+      for (let r = 0; r < rows.length; r++){
+        if (percent <= getPercent(data[r], options.min, options.max)){
+          rows[r].style.setProperty('--width', percent);
+        }
+      }
+      // stop the animation once bars are complete.
+      if (percent >= 100){
+        clearInterval(animateChart);
+        return;
+      }
+      percent += 0.1;
+    }, 1);
   };
 
 
@@ -27,5 +62,5 @@ $(document).ready(function(){
   // The stuff that isn't functions!
   const body = document.body;
 
-  drawBarChart([ 0, 25, 50, 75, 100 ], { min: 0, max: 100 }, body);
+  drawBarChart([ 42, 312, 160], { min: 42, max: 312 }, body);
 });
